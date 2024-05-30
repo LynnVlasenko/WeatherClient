178534238416cfb48df54a7ec5ef183e48026174
LynnVlasenko <lynn.vlasenko@gmail.com>
2024-05-30 17:21:10 +0200

    Added CoreData to the project
    - Add CoreData to the project
    - Setup CoreData Model
    - Add CoreData entities

---
+++ WeatherClient/WeatherClient/Source/Models/CoreData/CDWeatherInfo+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CDWeatherInfo+CoreDataClass.swift
+//  WeatherClient
+//
+//  Created by Алина Власенко on 30.05.2024.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CDWeatherInfo)
+public class CDWeatherInfo: NSManagedObject {
+
+}
